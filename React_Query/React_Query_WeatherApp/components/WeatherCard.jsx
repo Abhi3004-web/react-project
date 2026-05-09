49f421d7f8b331a39015167224d1ba960872f211
Abhi3004-web <abhijitranjan1@gmail.com>
2026-05-09 14:43:05 +0530

    changes

--- React_Query/React_Query_WeatherApp/components/WeatherCard.jsx
+++ React_Query/React_Query_WeatherApp/components/WeatherCard.jsx
@@ -2,14 +2,14 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Weather_Api } from "../Api/Weather_Api";
 
 function WeatherCard({ city }) {
-    const queryClient = useQueryClient();
+    //const queryClient = useQueryClient();
     const { isLoading, error, data } = useQuery({
         queryKey: ["weather", city],
         queryFn: () => Weather_Api(city),
         enabled: !!city, // only run when city exists
         staleTime: 300000, // 5 min cache
     });
-    
+    //console.log(queryClient.getQueriesData())
     if (!city) return <p>Search for a city</p>;
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>{error.message}</p>;
